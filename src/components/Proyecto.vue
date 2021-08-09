5d02daaffb18d575076c037c828c458200c598bc
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-08 20:06:57 -0500

    Creado el componente Proyecto que contiene la info necesaria de cada proyecto

---
+++ src/components/Proyecto.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-card class="mx-auto">
+      <v-card-title>{{ titulo }}</v-card-title>
+      <v-card-subtitle>{{ subTitulo }}</v-card-subtitle>
+      <v-card-actions>
+        <v-btn color="success" text> Pagina
+          <v-icon>mdi-web</v-icon>
+        </v-btn>
+        <v-spacer/>
+        <v-btn color="primary" text>
+          Repositorio
+          <v-icon>mdi-source-repository</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Proyecto',
+  components: {},
+  props: {
+    titulo: String,
+    subTitulo: String,
+    pagina: String,
+    repositorio: String,
+  },
+};
+</script>
+
+<style scoped></style>
