16a678f756f44a680277b327b9824e873d32d4f1
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-09 10:49:58 -0500

    Se agrego un carrusel de fotos

---
+++ src/components/Certificados.vue
@@ -0,0 +1,36 @@
+<template>
+	<v-carousel>
+		<v-carousel-item
+			v-for="(imh, i) in imagenes"
+			:key="i"
+			:src="imh.src"
+			reverse-transition="fade-transition"
+			transition="fade-transition"
+		>
+		</v-carousel-item>
+	</v-carousel>
+</template>
+
+<script>
+	export default {
+		name: 'Certificados',
+		data() {
+			return {
+				imagenes: [
+					{
+						src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+					},
+					{
+						src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+					},
+					{
+						src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+					},
+					{
+						src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+					},
+				],
+			};
+		},
+	};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,12 @@
 		<v-card max-width="1000" class="mx-auto mb-12">
 			<Info />
 		</v-card>
-		<v-card dark color="pink darken-5" max-width="800" class="mx-auto">
+		<v-card dark color="pink darken-5" max-width="800" class="mx-auto mb-12">
 			<ListadoProyectos />
+		</v-card>
+		<v-card dark color="pink darken-5" max-width="800" class="mx-auto">
+			<v-card-title>Certificados y cursos</v-card-title>
+			<Certificados />
 		</v-card>
 	</v-container>
 </template>
@@ -13,9 +17,10 @@
 	import Vue from 'vue';
 	import ListadoProyectos from '../components/ListadoProyectos.vue';
 	import Info from '../components/Info.vue';
+	import Certificados from '../components/Certificados.vue';
 
 	export default Vue.extend({
 		name: 'Home',
-		components: { ListadoProyectos, Info },
+		components: { ListadoProyectos, Info,Certificados },
 	});
 </script>
